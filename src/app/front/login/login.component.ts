18945fc4a1eb2477f50af49347e88d47e7d0f928
Samba <samba.dram.sd@gmail.com>
2019-10-25 16:50:46 +0200

    connexion deconnexion

--- src/app/front/login/login.component.ts
+++ src/app/front/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,15 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
   onConnexion($event, form) {
     $event.preventDefault();
     const credentials = form.value;
-    console.log(credentials);
 
-    //
+    // envoyer à un service qui va appeler notre projet node
+    this.authService.login(credentials).subscribe(result => {
+      if (result) {
+        this.router.navigate(['/admin/home']);
+      }
+    });
   }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,24 +1,49 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
-  // d'habitude =>CRUD
+  constructor(private http: HttpClient, private router: Router) {}
   url = 'http://localhost:5000/api/connexion';
+  // d'habitude =>CRUD
+
   login(credentials) {
     // appel à une API
     // ce n'est pas une promesse c'est un observable
     // opérateur d'observable
     return this.http.post(this.url, credentials).pipe(
       map(resp => {
-        return resp;
+        if (resp['token']) {
+          localStorage.setItem('token', resp['token']);
+          return true;
+        } else {
+          return false;
+        }
       })
     );
   }
 
-  logout() {}
+  logout() {
+    localStorage.removeItem('token');
+  }
+  logoutBack() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  isLoggedIn() {
+    let token = localStorage.getItem('token');
+    if (!token) {
+      return false;
+    }
+
+    const helper = new JwtHelperService();
+    const isExpired = helper.isTokenExpired(token);
+    return !isExpired;
+  }
 }
