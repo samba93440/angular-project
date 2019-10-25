c629b2b02ad677f231b8f208c6287de02e64b93a
Samba <samba.dram.sd@gmail.com>
2019-10-25 11:38:47 +0200

    definir ses routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './front/home/home.component';
+import { LoginComponent } from './front/login/login.component';
+import { NavbarComponent } from './front/navbar/navbar.component';
+import { NotFoundComponent } from './front/not-found/not-found.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { UserComponent } from './admin/user/user.component';
+import { ArticleComponent } from './admin/article/article.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    UserComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/home', component: DashboardComponent },
+      { path: 'admin/user', component: UserComponent },
+      { path: 'admin/article', component: ArticleComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
